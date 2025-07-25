746d8b3fc29f3c409f1afbc0d5efb9a5e51ba81a
Tom Jemmett <thomas.jemmett@nhs.net>
2025-07-25 11:42:27 +0100

    fixes type of params, renames to params_str

--- run_model/storage.py
+++ run_model/storage.py
@@ -8,12 +8,12 @@ import config
 
 
 def upload_params_to_blob(
-    params: dict, metadata: dict, credential: DefaultAzureCredential
+    params_str: str, metadata: dict, credential: DefaultAzureCredential
 ) -> None:
     client = BlobServiceClient(config.STORAGE_ENDPOINT, credential)
     container = client.get_container_client("queue")
     try:
-        container.upload_blob(f"{metadata['id']}.json", params, metadata=metadata)
+        container.upload_blob(f"{metadata['id']}.json", params_str, metadata=metadata)
         logging.info("params uploaded to queue")
     except ResourceExistsError:
         logging.warning("file already exists, skipping upload")
