c945000bd7160c87ac6674881e2a728c04d9919e
Tom Jemmett <thomas.jemmett@nhs.net>
2025-09-19 16:24:17 +0100

    disables the logs in python
    doesn't seem to work as expected from the host.json file

--- function_app.py
+++ function_app.py
@@ -1,7 +1,13 @@
+import logging
+
 import azure.functions as func
 
 from run_model import bp_run_model
 from status import bp_status
+
+logging.getLogger("azure").setLevel(logging.WARNING)
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
